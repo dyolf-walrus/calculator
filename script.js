018b395c03a631bdce79621806731a9a432dbabb
Allison <124654013+dyolf-walrus@users.noreply.github.com>
2023-07-05 17:26:28 -0400

    add and style UI

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let num1;
+let num2;
+let operator;
+
+function operate(a, b, c) {
+    if (c == '+') {
+        return add(a, b);
+    } else if (c == '-') {
+        return subtract(a, b);
+    } else if (c == '*') {
+        return multiply(a, b);
+    } else if (c == '/') {
+        return divide(a, b);
+    }
+}
+
+console.log(operate(10, 5, '/'))

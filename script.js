4dbac1ecaa6e8b2e6e740f1c95041eb705e42572
Allison <124654013+dyolf-walrus@users.noreply.github.com>
2023-08-03 03:29:12 -0400

    make calculator work, calculating with zero still needs fixed

--- script.js
+++ script.js
@@ -20,13 +20,13 @@ let operator;
 let displayValue = "";
 
 function operate(a, b, c) {
-    if (c == 'plus') {
+    if (c == '+') {
         return add(a, b);
-    } else if (c == 'minus') {
+    } else if (c == '-') {
         return subtract(a, b);
-    } else if (c == 'times') {
+    } else if (c == '*') {
         return multiply(a, b);
-    } else if (c == 'divide') {
+    } else if (c == '/') {
         return divide(a, b);
     }
 }
@@ -35,13 +35,67 @@ let display = document.getElementById("display")
 display.textContent = displayValue;
 
 const numInput = document.getElementsByClassName("number");
+const opInput = document.getElementsByClassName("operator");
+const clear = document.getElementById("clear");
+const equals = document.getElementById("equals");
 
 //to add numerical values to displayValue when 'number' buttons are clicked
 for (i = 0; i < numInput.length; i++) {
     let num = numInput[i];
+    if (num1 && !operator) {
+        displayValue = "";
+    }
     numInput[i].addEventListener('click', function() {
         console.log(num.dataset.value);
         displayValue = displayValue + num.dataset.value;
         display.textContent = displayValue;
     })
+}
+
+//to add operator to displayValue when 'operator' buttons are clicked
+for (i = 0; i < opInput.length; i++) {
+    let op = opInput[i];
+    opInput[i].addEventListener('click', function() {
+        if (!operator) {
+            operator = op.dataset.value;
+            displayValue = displayValue + " " + op.dataset.value + " ";
+            display.textContent = displayValue;
+        }
+        else if (operator) {
+            equaling();
+            operator = op.dataset.value;
+            displayValue = displayValue + " " + op.dataset.value + " ";
+            display.textContent = displayValue;
+        }
+    })
+}
+
+//AC button
+clear.addEventListener('click', function() {
+    displayValue = "";
+    num1 = undefined;
+    num2 = undefined;
+    operator = undefined;
+    display.textContent = displayValue;
+})
+
+//equals button
+equals.addEventListener('click', function() {
+    equaling();
+})
+
+function equaling() {
+    splitDisplay(displayValue);
+    if (num1 && num2 && operator) {
+        displayValue = operate(num1, num2, operator)
+        display.textContent = displayValue;
+        operator = undefined;
+    }
+}
+
+function splitDisplay(string) {
+    let arr = string.split(" ")
+    num1 = parseFloat(arr[0]);
+    num2 = parseFloat(arr[2]);
+    operator = arr[1];
 }
